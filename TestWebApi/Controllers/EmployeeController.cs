d7108960ebdbb1c84a1079249360c3228852a226
atip chao <atip/chaogunhar@cookcountyil.gov>
2019-07-28 11:39:28 -0500

    add Emp controler On branch master

---
+++ TestWebApi/Controllers/EmployeeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+using EmpDB;
+
+namespace TestWebApi.Controllers
+{
+    // use api
+    public class EmployeeController : ApiController
+    {
+        EmployeeDBEntities _db = new EmployeeDBEntities();
+
+        public IEnumerable<Employee> Get()
+        {
+            
+                return (_db.Employees.ToList());
+            
+        }
+        public Employee Get(int Id)
+        {
+            return (_db.Employees.Where(s => s.ID == Id).FirstOrDefault());
+        }
+    }
+}
