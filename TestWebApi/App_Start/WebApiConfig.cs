1c2f666093da4f0577852fd5659e7446c9e9a10a
atip chao <atip/chaogunhar@cookcountyil.gov>
2019-07-28 15:23:35 -0500

    @ On branch master

--- TestWebApi/App_Start/WebApiConfig.cs
+++ TestWebApi/App_Start/WebApiConfig.cs
@@ -25,6 +25,11 @@ namespace TestWebApi
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            // Set Json format
+            config.Formatters.JsonFormatter.SerializerSettings.Formatting = Newtonsoft.Json.Formatting.Indented;
+            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver(); 
+
         }
     }
 }
